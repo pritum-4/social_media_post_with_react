ecdb25187c2d2c81be1710f86e498c7ba5fb6dcd
Prayas Mahakur <104716871+pritum-4@users.noreply.github.com>
2024-03-20 17:10:46 +0530

    Create FollowButton.js

---
+++ FollowButton.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function FollowButton() {
+  const [following, setFollowing] = useState(false);
+  return (
+    <div
+      id="follow-button"
+      className={following ? "following" : "not-following"}
+      onClick={() => setFollowing(!following)}
+    >
+      {following == true ? "Following" : "Follow"}
+    </div>
+  );
+}
